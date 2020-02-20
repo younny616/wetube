95df8c874344d28693dc1ad60cb6ef80f2cbb953
younny616 <younny616@gmail.com>
2020-02-20 14:55:46 +0900

    server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hi!");
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+function handleHome() {
+    console.log("Hi from home!!");
+}
+
+app.get("/", handleHome);
+
+app.listen(PORT, handleListening);
