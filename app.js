c97691209da8604fb9ae70b86b4041c7ec0fb3de
younny616 <younny616@gmail.com>
2020-02-24 23:00:38 +0900

    controller and pug

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 // const express = require('express');
 
 import express from "express";
-//import morgan from "morgan";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -16,6 +20,16 @@ const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);*/
+app.use(helmet());
+app.set("view engine", "pug");
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(morgan("dev"));
+
+
+app.use(localsMiddleware);
+
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
