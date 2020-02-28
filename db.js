80386cf8ed6a3c747d46a14c932e72ade0d82901
younny616 <younny616@gmail.com>
2020-02-28 09:23:50 +0900

    video

---
+++ db.js
@@ -0,0 +1,62 @@
+export const videos = [
+    {
+        id:324393,
+        title: 'Video awesome',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: 'https://archive.org/details/BigBuckBunny_124',
+        creator: {
+            id: 121212,
+            name: "ny",
+            email: "younny616@gmail.com"
+        }
+    },
+    {
+        id:123456,
+        title: 'Video super',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: 'https://archive.org/details/BigBuckBunny_124',
+        creator: {
+            id: 121212,
+            name: "ny",
+            email: "younny616@gmail.com"
+        }
+    },
+    {
+        id:567891,
+        title: 'Video hello',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: 'https://archive.org/details/BigBuckBunny_124',
+        creator: {
+            id: 121212,
+            name: "ny",
+            email: "younny616@gmail.com"
+        }
+    },
+    {
+        id:555555,
+        title: 'Video ny',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: 'https://archive.org/details/BigBuckBunny_124',
+        creator: {
+            id: 121212,
+            name: "ny",
+            email: "younny616@gmail.com"
+        }
+    },
+    {
+        id:111111,
+        title: 'Video perfect',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: 'https://archive.org/details/BigBuckBunny_124',
+        creator: {
+            id: 121212,
+            name: "ny",
+            email: "younny616@gmail.com"
+        }
+    }
+]
