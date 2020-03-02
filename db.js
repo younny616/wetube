a414b99dadacf492308d581b9a66ba38432e7e2f
younny616 <younny616@gmail.com>
2020-03-02 22:36:28 +0900

    db

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ export const videos = [
         title: 'Video awesome',
         description: 'This is something I love',
         views: 24,
-        videoFile: 'https://archive.org/details/BigBuckBunny_124',
+        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
         creator: {
             id: 121212,
             name: "ny",
@@ -16,7 +16,7 @@ export const videos = [
         title: 'Video super',
         description: 'This is something I love',
         views: 24,
-        videoFile: 'https://archive.org/details/BigBuckBunny_124',
+        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
         creator: {
             id: 121212,
             name: "ny",
@@ -28,7 +28,7 @@ export const videos = [
         title: 'Video hello',
         description: 'This is something I love',
         views: 24,
-        videoFile: 'https://archive.org/details/BigBuckBunny_124',
+        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
         creator: {
             id: 121212,
             name: "ny",
@@ -40,7 +40,7 @@ export const videos = [
         title: 'Video ny',
         description: 'This is something I love',
         views: 24,
-        videoFile: 'https://archive.org/details/BigBuckBunny_124',
+        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
         creator: {
             id: 121212,
             name: "ny",
@@ -52,7 +52,7 @@ export const videos = [
         title: 'Video perfect',
         description: 'This is something I love',
         views: 24,
-        videoFile: 'https://archive.org/details/BigBuckBunny_124',
+        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
         creator: {
             id: 121212,
             name: "ny",
