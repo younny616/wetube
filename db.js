538a9d242c2e0cc32f0f2aca313cabd2ca62b43c
younny616 <younny616@gmail.com>
2020-03-03 21:32:00 +0900

    db

--- db.js
+++ db.js
@@ -1,62 +1,19 @@
-export const videos = [
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL, 
     {
-        id:324393,
-        title: 'Video awesome',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-        creator: {
-            id: 121212,
-            name: "ny",
-            email: "younny616@gmail.com"
-        }
-    },
-    {
-        id:123456,
-        title: 'Video super',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-        creator: {
-            id: 121212,
-            name: "ny",
-            email: "younny616@gmail.com"
-        }
-    },
-    {
-        id:567891,
-        title: 'Video hello',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-        creator: {
-            id: 121212,
-            name: "ny",
-            email: "younny616@gmail.com"
-        }
-    },
-    {
-        id:555555,
-        title: 'Video ny',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-        creator: {
-            id: 121212,
-            name: "ny",
-            email: "younny616@gmail.com"
-        }
-    },
-    {
-        id:111111,
-        title: 'Video perfect',
-        description: 'This is something I love',
-        views: 24,
-        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-        creator: {
-            id: 121212,
-            name: "ny",
-            email: "younny616@gmail.com"
-        }
+        useNewUrlParser: true, 
+        useFindAndModify:false
     }
-]
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("connected to db");
+const handleError = () => console.log(`connected to error: ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleOpen);
