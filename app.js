7ee9b0ffd0ecbfd615beae700609256ad9006c5f
younny616 <younny616@gmail.com>
2020-03-13 01:55:49 +0900

    webpack passport

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.get("/", handleHome);
 app.get("/profile", handleProfile);*/
 app.use(helmet());
 app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }));
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,10 @@
+import routes from "../routes";
+import User from "../models/User";
+
 export const getJoin = (req, res) => {
     res.render("join", {pageTitle: "Join"});
 };
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res) => {
     const {
         body: {name, email, password, password2}
     } = req;
@@ -9,14 +12,26 @@ export const postJoin = (req, res) => {
         res.status(400);
         res.render("join", {pageTitle: "Join"});
     } else {
+        try{
+            const user = await User({
+                name, email
+            });
+            await User.register(user, password);
+        } catch(error) {
+            console.log(error);
+        }
         res.redirect(routes.home);
     }
     
 };
 
+export const getLogin = (req, res) => res.render("login", {pageTitle: "Login"});
+export const postLogin = (req, res) => res.redirect(routes.home);
 
-export const login = (req, res) => res.render("login", {pageTitle: "Login"});
-export const logout = (req, res) => res.render("logout", {pageTitle: "Logout"});
+export const logout = (req, res) => {
+    // To do: process log out
+    res.redirect(routes.home);
+}
 export const userDetail = (req, res) => res.render("userDetail", {pageTitle: "User Detail"});
 export const editProfile = (req, res) => res.render("editProfile", {pageTitle: "Edit Profile"});
 export const changePassword = (req, res) => res.render("changePassword", {pageTitle: "Change Password"});
